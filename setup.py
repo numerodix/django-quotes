d863159207f5349873ec5b7ba9a8fe4588ff0902
Martin Matusiak <numerodix@gmail.com>
2012-05-06 22:33:40 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ setup(
     version='0.1',
     description='Random quotes for django',
     long_description=open('README.md').read(),
-    # Get more strings from http://www.python.org/pypi?:action=list_classifiers
     author='Martin Matusiak',
     author_email='numerodix@gmail.com',
     url='https://numerodix@github.com/numerodix/django-quotes.git',
     download_url='https://numerodix@github.com/numerodix/django-quotes.git',
-    license='BSD',
+    license='GPL',
 )
